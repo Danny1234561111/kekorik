e687d429ece929be6657c04fc564c7b98d0e226b
Danny1234561111 <shornikovdaniil1111@gmail.com>
2024-04-17 09:42:06 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='mtraker',
-   version='1.0',
-   description='My first library.',
+   name='mtracker',
+   version='0.5',
+   description='Legendary the hardest bot for game The Battle of Polytopia',
    license='MIT',
-   author='Danny1234561111',
-   author_email='shornikovdaniil1111@gmail.com',
-   url='https://github.com/Danny1234561111/kekorik.git',
-   packages=find_packages(exclude=['mtracker']),,
+    author='Danny1234561111',
+    author_email='shornikovdaniil1111@gmail.com',
+    url='https://github.com/Danny1234561111/kekorik.git',
+   packages=find_packages(exclude=['mtracker']),
    install_requires=[], # it is empty since we use standard python library
    extras_require={
         'test': [
