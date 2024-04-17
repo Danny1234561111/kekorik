e3b53569da8a0e742a2b2b00e89a9d4bb0a9e179
Danny1234561111 <154524524+Danny1234561111@users.noreply.github.com>
2024-04-17 09:19:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
    author='Danny1234561111',
    author_email='shornikovdaniil1111@gmail.com',
    url='https://github.com/Danny1234561111/kekorik.git',
-   packages=find_packages(exclude=['mtracker']),,
+   packages=find_packages(exclude=['mtracker']),
    install_requires=[], # it is empty since we use standard python library
    extras_require={
         'test': [
