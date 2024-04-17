3e11f789ae72c9b32d1a175313f2f5566dc535d1
Danny1234561111 <shornikovdaniil1111@gmail.com>
2024-04-17 09:46:49 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
    author = 'Danny1234561111',
    author_email = 'shornikovdaniil1111@gmail.com',
    url = 'https://github.com/Danny1234561111/kekorik.git',
-   packages=find_packages(exclude=['tests']),
+   packages=find_packages(exclude=['test']),
    install_requires=[], # it is empty since we use standard python library
    extras_require={
         'test': [
