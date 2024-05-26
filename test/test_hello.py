fdf610791dde70e49963bee957cf01d00e0bb07a
Danny1234561111 <154524524+Danny1234561111@users.noreply.github.com>
2024-05-26 19:41:59 +0800

    Update test_hello.py

--- test/test_hello.py
+++ test/test_hello.py
@@ -1,6 +1,6 @@
 import pytest
 from hello.hello import hello
 def test_hello():
-    assert hello("Василий")=="Hello"+ "Василий"+"!"
-    assert hello(8)=="Hello 8!"
-    assert hello("fffff")=="Hello "+"fffff"+"!"
+    assert hello("Василий")=="hello"+ " Василий"+"!"
+    assert hello(8)=="hello 8!"
+    assert hello("fffff")=="hello "+"fffff"+"!"
